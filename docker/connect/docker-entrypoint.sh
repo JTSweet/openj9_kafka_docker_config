64fa5cfc990fe2d2aa996a8f0f562ee268421551
Travis Sweet <jtsweet@confluent.io>
2025-07-16 02:18:59 -0400

    Update docker-entrypoint.sh

--- docker/connect/docker-entrypoint.sh
+++ docker/connect/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # If CONNECT_REST_ADVERTISED_HOST_NAME is not set, dynamically determine the container's IP.
 # This is crucial for distributed mode in containerized environments.
-if; then
+if -z "${CONNECT_REST_ADVERTISED_HOST_NAME}"; then
     export CONNECT_REST_ADVERTISED_HOST_NAME=$(hostname -i)
 fi
 echo "Advertised REST Host Name: ${CONNECT_REST_ADVERTISED_HOST_NAME}"
